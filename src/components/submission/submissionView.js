09734c67571a9cacc99ee5a531d4018722228775
Joachem Trinidad <joakimtrinidad234@gmail.com>
2024-06-18 15:58:31 +0800

    commit

--- src/components/submission/submissionView.js
+++ src/components/submission/submissionView.js
@@ -8,8 +8,10 @@ import { variables } from '../../variables';
 import { base64pdf } from '../../vblob';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-// import pdf1 from '../../dummy.pdf'
 import "react-pdf/dist/esm/Page/TextLayer.css"; 
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { Document, Page,pdfjs } from 'react-pdf'; 
 
@@ -18,7 +20,9 @@ import { Document, Page,pdfjs } from 'react-pdf';
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`; 
 
     const location = useLocation();
-    const data = location.state.data;  
+    const data = location.state.data;
+
+    // console.log(location.state.EmpID);
 
     const { employeeId } = useParams();
     const [showModal, setShowModal] = useState(false);
@@ -29,6 +33,20 @@ import { Document, Page,pdfjs } from 'react-pdf';
 
     const [pdf, setPdf] = useState([]);
     const [pdfResubmit, setPdfResubmit] = useState([]);
+
+    const [resubmitFiles, setResubmitFiles] = useState([]);
+  
+    // const handleResubmitPDF = (e) => {
+    //   setResubmitFiles([...resubmitFiles, ...e.target.files]);
+    // };
+
+    const handleResubmitPDF = (e, requirementName, PdfFileID) => {
+      // Combine the file with its corresponding RequirementName
+      const thisSubmissionID = data.SubmissionID;
+
+      const fileWithRequirementName = { file: e.target.files[0], requirementName, PdfFileID, thisSubmissionID};
+      setResubmitFiles([...resubmitFiles, fileWithRequirementName]);
+    };
     
     // Converts base64 to pdf
     const convertToPDF = (base64) => {
@@ -105,35 +123,142 @@ import { Document, Page,pdfjs } from 'react-pdf';
           console.error('Error uploading PDF:', error);
         }
       }; 
-  
 
-    const handleFormSubmit = async (e) => {
-      e.preventDefault();
-      // try {
-      //   const response = await fetch(variables.API_URL + 'UploadEmp/' + employeeId, {
-      //     method: 'PUT',
-      //     headers: {
-      //       'Content-Type': 'application/json'
-      //     },
-      //     body: JSON.stringify(employeeData)
-      //   });
-      //   if (!response.ok) {
-      //     throw new Error('Failed to update employee');
-      //   }
-      //   // Handle successful update
-      //   console.log('Employee updated successfully');
-      // } catch (error) {
-      //   console.error('Error updating employee:', error);
-      // }
-    }; 
+      const handleFormSubmit = async (e) => {
+        e.preventDefault();
+
+        // Check if any required field is empty
+        if (resubmitFiles.length === 0) {
+          // If the file is not selected, show a warning toast
+          toast.warn('Please select a file to submit', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          return; // Stop form submission
+        }
+      
+        const formData = new FormData();
+        resubmitFiles.forEach((file) => {
+          formData.append('newPDF', file.file); // Append only the file
+          formData.append('requirementName', file.requirementName.toString()); // Append the requirementName
+          formData.append('PdfFileID', file.PdfFileID); // Append the PdfFileID
+          formData.append('SubmissionID', file.thisSubmissionID); // Append the PdfFileID
+        });
+
+
+        try {
+          const response = await fetch('http://localhost:5000/resubmitPDF', {
+            method: 'POST',
+            body: formData,
+          }); 
+          if (response.ok) {
+            const jsonResponse = await response.json();
+
+            console.log(jsonResponse.message);
+            
+            setTimeout(() => {
+              window.location.reload();
+            }, 2500);
+            
+            // Emit success toast
+            toast.success('Submitted Successfully', {
+              position: "bottom-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+            setResubmitFiles([]);
+          } else {
+            console.error('Failed to upload PDF:', response.statusText);
+              toast.error('Failed to Submit', {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+          }
+        } catch (error) {
+          console.error('Error uploading PDF:', error);
+        }
+      };
+      
   
     // Modal functions
-    const handleButtonClick = (base64) => { 
+    const handleButtonClick = (base64, e) => { 
+      e.preventDefault();
+      console.log(e);
       convertToPDF(base64);
       setShowModal(true);
     };
+
     const handleCloseModal = () => {
       setShowModal(false);
+    };
+    
+    
+    const handleCancel = async () => {
+        const thisAction = "Cancelled"; 
+
+        const formData = new FormData();
+        formData.append('thisAction', thisAction);
+        formData.append('thisSubmissionID', data.SubmissionID);
+
+        try {
+          const response = await fetch('http://localhost:5000/UpdateRequest', {
+            method: 'POST',
+            body: formData,
+          }); 
+          if (response.ok) {
+            const jsonResponse = await response.json();
+
+            console.log(jsonResponse.message);
+            
+            // Emit success toast
+            toast.success('Submitted Successfully', {
+              position: "bottom-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+
+            setTimeout(() => {
+              window.location.href = '/submissions';
+            }, 2300);
+
+          } else {
+            console.error('Failed to upload PDF:', response.statusText);
+              toast.error('Failed to Submit', {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+          } 
+        } catch (error) {
+        console.error('Error updating failed:', error);
+      }
     };
  
     
@@ -165,20 +290,25 @@ import { Document, Page,pdfjs } from 'react-pdf';
                     <div className="tab-content">
                       <div className="tab-pane fade show active" id="personalDetails" role="tabpanel" aria-labelledby="personalDetails-tab">
                           {/* Personal Details Form */}
-                      <div className="container">
+                            <div className="container">
                               <div className="justify-content-center">
-                                <div > 
-                                <div className="d-flex justify-content-between">
+                                <div> 
+                                  <div className="d-flex justify-content-between">
                                     <label>{data.Name}</label>
                                     <label>{data.DateTime}</label>
                                     <label>{data.TurnAround} Days</label>
                                     <label>{data.Status}</label>
-                                    <Button>Confirm</Button>
-                                </div>
+                                      {data.Status === 'Pending' ? (
+                                          <Button type="button" onClick={handleCancel}>Cancel</Button>
+                                      ) : (
+                                          <span></span>
+                                      )}
+                                    
+                                  </div>
                                 </div> 
                               </div> 
-                      </div>
-                      <br/> 
+                            </div>
+                        <br/> 
                       </div> 
                       {/* Add more tab content here */}
                       </div>
@@ -232,51 +362,53 @@ import { Document, Page,pdfjs } from 'react-pdf';
                 {/* Page content ends here */}
 
                 {/* page content begin here */}  
-                {pdf && pdf.map((pdf, index) =>
-                <div className="container-fluid">
+                {pdf && pdf.map((pdfItem, index) =>
+                  <div className="container-fluid" key={index}>
                     <div className="row justify-content-center">
-                        <div className="col-xl-8 col-lg-7">
-                            <div className="card shadow mb-4">
-                                {/* Card Header - New Hire Upload */}
-                                <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                  <h6 className="m-0 font-weight-bold text-primary">{pdf.RequirementName}</h6> 
-                                  <h6 className="m-0 font-weight-bold" style={{color: 'red'}}>{pdf.Resubmit?'Resubmit':''}</h6> 
+                      <div className="col-xl-8 col-lg-7">
+                        <div className="card shadow mb-4">
+                          {/* Card Header - New Hire Upload */}
+                          <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                            <h6 className="m-0 font-weight-bold text-primary">{pdfItem.RequirementName}</h6>
+                            
+                            <h6 className="m-0 font-weight-bold" style={{ color: 'red' }}>{pdfItem.Resubmit ? 'Resubmit' : ''}</h6>
+                          </div>
+                          {/* Card Body - New Hire Options */}
+                          <div className="card-body">
+                            <div className="tab-content">
+                              <div className="card-body">
+                                <div className="d-flex justify-content-between align-items-center">
+                                  <div>
+                                    <button onClick={(e) => handleButtonClick(pdfItem.PdfData, e)}>
+                                      View PDF
+                                    </button>
+                                    <button onClick={() => convertAndDownloadPDF(pdfItem.PdfData, pdfItem.FileName)} className='btnClose'>
+                                      Download
+                                    </button>
+                                    <label>{pdfItem.FileName}</label>
+                                  </div>
+                                  <label>Date Submitted: {pdfItem.UploadDate}</label>
                                 </div>
-                                {/* Card Body - New Hire Options */}
-                                <div className="card-body">
-                                    <div className="tab-content">
-                                        <div className="card-body">
-                                            <div className="d-flex justify-content-between align-items-center">
-                                              <div> 
-                                                <button onClick={() => handleButtonClick(pdf.PdfData)}>
-                                                  View PDF
-                                                </button>
-                                                <button onClick={() => convertAndDownloadPDF(pdf.PdfData,pdf.FileName)} className='btnClose'>
-                                                  Download
-                                                </button>
-                                                <label>{pdf.FileName}</label>
-                                              </div>
-                                              <label>Date Submitted: {pdf.UploadDate}</label>  
-                                            </div>
- 
-                                            {/* For Resubmission */}
-                                            {pdf.Resubmit === 1 &&
-                                              <div className="d-flex justify-content-between">
-                                                  {pdf.EmpResubmitted === 0 &&
-                                                  <div className="d-flex justify-content-left">
-                                                      <input type="file" className="input-file" aria-describedby="fileHelp"/> 
-                                                  </div> }
-                                                  <label>Reason: {pdf.ResubmitReason}</label>
-                                              </div>
-                                            }
-                                        </div>
-                                    </div>
-                                </div>
+
+                                {/* For Resubmission */}
+                                {pdfItem.Resubmit === 1 &&
+                                  <div className="d-flex justify-content-between">
+                                    {pdfItem.EmpResubmitted === 0 &&
+                                      <div className="d-flex justify-content-left">
+                                        {/* Pass pdf.RequirementName to handleResubmitPDF */}
+                                        <input type="file" className="input-file" aria-describedby="fileHelp" onChange={(e) => handleResubmitPDF(e, pdfItem.RequirementName, pdfItem.PdfFileID, location.state.EmpID, data.SubmissionID)} />
+                                      </div>}
+                                    <label>Reason: {pdfItem.ResubmitReason}</label>
+                                  </div>
+                                }
+                              </div>
                             </div>
+                          </div>
                         </div>
+                      </div>
                     </div>
-                </div>
-                )} 
+                  </div>
+                )}
                 {/* Page content ends here */}
 
                 {/* page content begin here */}
@@ -318,37 +450,12 @@ import { Document, Page,pdfjs } from 'react-pdf';
                     </div>
                 </div> */}
                 {/* Page content ends here */} 
+                {data.Status === 'Resubmit' ? (
+                  <button type="submit" className="btn btn-primary d-block mx-auto mb-4">Submit</button>
+                ) : (
+                  <span></span>
+                )}
                 
-                {/* page content begin here */}
-                <div className="container-fluid">
-                    <div className="row justify-content-center">
-                        <div className="col-xl-8 col-lg-7">
-                            <div className="card shadow mb-4">
-                                {/* Card Header - New Hire Upload */}
-                                <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 className="m-0 font-weight-bold text-primary">Remark</h6>
-                                </div>
-                                {/* Card Body - New Hire Options */}
-                                <div className="card-body">
-                                    <div className="tab-content">
-                                        <div className="card-body loan-row"> 
-                                            <div className="form-group"> <textarea 
-                                              className="form-control" 
-                                              id="remark" 
-                                              name="remark"
-                                              rows="3" 
-                                              style={{ resize: "vertical" }}
-                                            />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* Page content ends here */}
-                <button type="submit" className="btn btn-primary d-block mx-auto">Submit</button>
                 </form>
               </div>
 
@@ -390,9 +497,20 @@ import { Document, Page,pdfjs } from 'react-pdf';
               </div>
               <Footer />
           </div>
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
       </div>
   );
 }
 
 export default RequestView;
-
